70393438d6760aaa7e16d889abc434b5dffb47c6
Christopher Stardust <yeti.detective@proton.me>
2025-12-15 15:15:27 -0500

    refactor table to child component

---
+++ src/app/Components/AdvocatesTable.tsx
@@ -0,0 +1,42 @@
+/**
+ * Displays advocates' first name, last name, city, degree, specialties,
+ * years of experience, and phone number
+ *
+ * @param advocates - list of Advocate data to display
+ */
+export function AdvocatesTable({ advocates }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>City</th>
+          <th>Degree</th>
+          <th>Specialties</th>
+          <th>Years of Experience</th>
+          <th>Phone Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        {advocates.map((advocate) => {
+          return (
+            <tr>
+              <td>{advocate.firstName}</td>
+              <td>{advocate.lastName}</td>
+              <td>{advocate.city}</td>
+              <td>{advocate.degree}</td>
+              <td>
+                {advocate.specialties.map((s) => (
+                  <div>{s}</div>
+                ))}
+              </td>
+              <td>{advocate.yearsOfExperience}</td>
+              <td>{advocate.phoneNumber}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import {AdvocatesTable} from "./Components/AdvocatesTable";
 
 export default function Home() {
   const [advocates, setAdvocates] = useState([]);
@@ -56,38 +57,7 @@ export default function Home() {
       </div>
       <br />
       <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <AdvocatesTable advocates={filteredAdvocates} />
     </main>
   );
 }
