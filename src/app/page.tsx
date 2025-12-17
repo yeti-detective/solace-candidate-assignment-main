8dd02468bbc8d767c2b90b74b1d55f78a6ccd21f
Christopher Stardust <yeti.detective@proton.me>
2025-12-17 10:53:25 -0500

    implement pagination on front end. now search is bad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,38 @@
 import { SolaceCandidateAssignmentQueryClientProvider } from "./api/QueryClientProvider";
 import { AdvocatesTable } from "./Components/AdvocatesTable";
 import { SearchHeader } from "./Components/SearchHeader";
+import { Pagination } from "./Components/Pagination";
 import { useFilteredAdvocates } from "./api/queryHooks/useAdvocates";
 
 /**
  * Home Page component for the Solace Candidate Assignment app
  */
 function Home() {
-  const {filteredAdvocates, onSearchTermChange, resetSearch} = useFilteredAdvocates();
+  const {
+    filteredAdvocates,
+    onSearchTermChange,
+    resetSearch,
+    pagination,
+    goToNextPage,
+    goToPreviousPage,
+    goToPage,
+  } = useFilteredAdvocates();
 
   return (
     <main style={{ margin: "24px" }}>
       <SearchHeader onClick={resetSearch} onChange={onSearchTermChange}>
         <AdvocatesTable advocates={filteredAdvocates} />
+        {pagination && (
+          <Pagination
+            page={pagination.page}
+            totalPages={pagination.totalPages}
+            hasNextPage={pagination.hasNextPage}
+            hasPreviousPage={pagination.hasPreviousPage}
+            onNextPage={goToNextPage}
+            onPreviousPage={goToPreviousPage}
+            onGoToPage={goToPage}
+          />
+        )}
       </SearchHeader>
     </main>
   );
